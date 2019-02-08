d5167c7e3ce198a570d610c0acb0001d6e943bee
natashadickerman <tasha.dickerman@students.pcci.edu>
2019-02-08 11:00:37 -0600

    bootstrap carousel

--- movies.js
+++ movies.js
@@ -6,12 +6,14 @@ window.addEventListener('load', () => {
     let topRated = document.getElementById("topRated");
     let upcoming = document.getElementById("upcoming");
 
+    addSliderImages();
+
     getMainImage(popularImage);
     getMainImage(topRated);
     getMainImage(upcoming);
 
-    getPopular();
-    getTopRated();
+    // getPopular();
+    // getTopRated();
     getUpcoming();
 })
 
@@ -53,6 +55,21 @@ function getUpcoming(){
     xhr.onreadystatechange = function () {
       if(xhr.readyState === 4 && xhr.status === 200) {
         console.log(xhr.responseText);
+
+
+        // Find the properties attached to the object
+        let responseJSON = xhr.response;
+        let responseObj = JSON.parse(responseJSON);
+    
+        var getKeys = function(responseObj){
+            var keys = [];
+            for(var key in responseObj){
+               keys.push(key);
+            }
+            return keys;
+         }
+    
+        console.log(getKeys(responseObj));
       }
     };
 
@@ -60,9 +77,31 @@ function getUpcoming(){
     xhr.open('get', 'https://api.themoviedb.org/3/movie/upcoming?api_key=14e4604305730956fb72f6204af61a7e&language=en-US&page=1')
 
     // send the request
-    xhr.send();
+    xhr.send();  
+}
+
+function parseJSON(obj){
+
 }
 
 function getMainImage(image){
     image.src = 'https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg';
+}
+
+function addSliderImages(){
+    $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+        var next = $(this).next();
+        if (!next.length) {
+          next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+      
+        for (var i=0;i<3;i++) {
+          next=next.next();
+          if (!next.length) {
+            next = $(this).siblings(':first');
+          }
+          next.children(':first-child').clone().appendTo($(this));
+        }
+      });
 }
